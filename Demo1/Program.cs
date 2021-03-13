b506661fcffae88b4455eea684f13481d44f152c
Zimmer Dipper <zdq4444@outlook.com>
2021-03-13 23:17:47 +0800

    Day1

--- Demo1/Program.cs
+++ Demo1/Program.cs
@@ -6,7 +6,23 @@ namespace Demo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] number = getArray();
+            foreach (var item in number)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+        public static int[] getArray()
+        {
+            int[] tempArray = new int[20];
+            tempArray[0]= 1;
+            tempArray[1]= 1;
+            for (int i = 2; i < tempArray.Length; i++)
+            {
+                tempArray[i] = tempArray[i - 1] + tempArray[i - 2];
+            }
+
+            return tempArray;
         }
     }
 }
